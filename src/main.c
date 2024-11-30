90f0497d717a277ec790464ee7657f731fba9bf8
Trisha <trisha2305341292@diu.edu.bd>
2024-12-01 01:21:24 +0600

    add

--- src/main.c
+++ src/main.c
@@ -1,7 +1,5 @@
 #include "banking_system.h"
 #include "banking_system_display.c"
-#include "banking_rules.c"
-#include "generate_transaction_id.c"
 
 #include "admin_portal/admin_login_portal.c"
 #include "admin_portal/admin_deposit_funds.c"
@@ -12,17 +10,22 @@
 #include "admin_portal/admin_withdraw_funds.c"
 #include "admin_portal/admin_view_transaction_history.c"
 #include "admin_portal/admin_delete_Account.c"
-#include "admin_portal/admin_update_customer_info.c"
+#include "admin_portal/admin_account_update.c"
 
+#include "generate_transaction_id.c"
+#include "banking_rules.c"
 
 #include "customer_portal/customer_menu_handle.c"
 #include "customer_portal/customer_login_process.c"
+
+
 
 int is_integer(const char *str)
 {
     for (int i = 0; str[i] != '\0'; i++)
     {
-        
+        /*isdigit একটি C স্ট্যান্ডার্ড লাইব্রেরির ফাংশন, যা নির্দিষ্ট কোনো ক্যারেক্টার ডিজিট (0-9)
+         কিনা তা যাচাই করতে ব্যবহৃত হয়। এটি ctype.h হেডার ফাইলে থাকে।*/
         if (!isdigit(str[i]))
         {
             return 0; // False if any character is not a digit
@@ -55,7 +58,7 @@ int main()
     printf(CYAN "********************************************\n\n\n" RESET);
 
     int choice, logged_in = 0, customer_index = -1;
-    User user;      
+    User user;      // Declare the User struct for logged-in customer
     char input[10]; // Buffer to store user input as a string
 
     while (1)
@@ -65,7 +68,7 @@ int main()
         fgets(input, sizeof(input), stdin); // Read input as a string
 
         // Remove the newline character from input if it exists
-        input[strcspn(input, "\n")] = 0; // strcspn একটি স্ট্রিং ফাংশন যা string.h লাইব্রেরি থেকে আসে।
+        input[strcspn(input, "\n")] = 0;
 
         if (is_integer(input))
         {
@@ -73,7 +76,7 @@ int main()
         }
         else
         {
-            printf(BRED "Invalid choice. Please enter a number.\n" RESET);
+            printf(BRED"Invalid choice. Please enter a number.\n"RESET);
             continue; // Skip to the next iteration if input is invalid
         }
 
@@ -83,89 +86,88 @@ int main()
         {
         case 1: // Admin Login
             logged_in = admin_login();
-
-            while (logged_in)
+            if (logged_in)
             {
-                // double balance = get_branch_account_balance();
-                // printf("Current branch account balance: %.2f\n", balance);
-                display_banking_system_features();
-                printf("Enter your choice: ");
-                scanf("%d", &choice);
-                getchar(); // To consume the leftover newline character from scanf | বাফার পরিষ্কার করার জন্য
-                
+                while (logged_in)
+                {
+                    // double balance = get_branch_account_balance();
+                    // printf("Current branch account balance: %.2f\n", balance);
+                    display_banking_system_features();
+                    printf("Enter your choice: ");
+                    scanf("%d", &choice);
+                    getchar(); // To consume the leftover newline character from scanf
 
-                switch (choice)
-                {
-                case 1:
-                    register_account();
-                    break;
-                case 2:
-                    view_transaction_history();
-                    break;
-                case 3: // Search Account by Account Number
-                {
-                    char account_number[ACCOUNT_NUMBER_LENGTH + 1]; //+1 রাখা হয়েছে, কারণ স্ট্রিং-এর শেষে \0 (null terminator) রাখতে অতিরিক্ত এক বাইট প্রয়োজন হয়।
-                    printf(BLUE "Enter the account number: " RESET);
-                    fgets(account_number, ACCOUNT_NUMBER_LENGTH + 1, stdin);
-                    remove_newline(account_number);
-                    search_and_view_account(account_number);
-                    break;
-                }
-                case 4:
-                {
-                    char account_number_for_balance[ACCOUNT_NUMBER_LENGTH + 1];
-                    printf("Enter the account number: ");
-                    fgets(account_number_for_balance, ACCOUNT_NUMBER_LENGTH + 1, stdin);
-                    remove_newline(account_number_for_balance);
-                    search_and_view_balance(account_number_for_balance);
-                    break;
-                }
-                case 5:
-                {
-                    char account_number_for_deposit_funds[ACCOUNT_NUMBER_LENGTH + 1];
-                    printf("Enter the account number: ");
-                    fgets(account_number_for_deposit_funds, ACCOUNT_NUMBER_LENGTH + 1, stdin);
-                    remove_newline(account_number_for_deposit_funds);
-                    deposit_funds(account_number_for_deposit_funds);
-                    break;
-                }
-                case 6:
-                {
-                    char account_number_for_withdraw_funds[ACCOUNT_NUMBER_LENGTH + 1];
-                    printf("Enter the account number: ");
-                    fgets(account_number_for_withdraw_funds, ACCOUNT_NUMBER_LENGTH + 1, stdin);
-                    remove_newline(account_number_for_withdraw_funds);
-                    withdraw_funds(account_number_for_withdraw_funds);
-                    break;
-                }
-                case 7:
-                {
-                    admin_transfer_funds();
-                    break;
-                }
-                case 8:
-                {
-                    char account_number_for_account_delete[ACCOUNT_NUMBER_LENGTH + 1];
-                    printf("Enter the account number: ");
-                    fgets(account_number_for_account_delete, ACCOUNT_NUMBER_LENGTH + 1, stdin);
-                    remove_newline(account_number_for_account_delete);
-                    deleteAccount(account_number_for_account_delete);
-                    break;
-                }
-                case 9:
-                {
-                    char account_number_for_account_update[ACCOUNT_NUMBER_LENGTH + 1];
-                    printf("Enter the account number: ");
-                    fgets(account_number_for_account_update, ACCOUNT_NUMBER_LENGTH + 1, stdin);
-                    remove_newline(account_number_for_account_update);
-                    update_customer_info(account_number_for_account_update);
-                    break;
-                }
-                case 10:
-                    logged_in = 0; // Logout
-                    break;
-                default:
-                    printf("Invalid choice.\n");
+                    switch (choice)
+                    {
+                    case 1:
+                        register_account();
+                        break;
+                    case 2:
+                        view_transaction_history();
+                        break;
+                    case 3: // Search Account by Account Number
+                    {
+                        char account_number[ACCOUNT_NUMBER_LENGTH + 1]; //+1 রাখা হয়েছে, কারণ স্ট্রিং-এর শেষে \0 (null terminator) রাখতে অতিরিক্ত এক বাইট প্রয়োজন হয়।
+                        printf(BLUE "Enter the account number: " RESET);
+                        fgets(account_number, ACCOUNT_NUMBER_LENGTH + 1, stdin);
+                        remove_newline(account_number);
+                        search_and_view_account(account_number);
+                        break;
+                    }
+                    case 4:
+                    {
+                        char account_number_for_balance[ACCOUNT_NUMBER_LENGTH + 1];
+                        printf("Enter the account number: ");
+                        fgets(account_number_for_balance, ACCOUNT_NUMBER_LENGTH + 1, stdin);
+                        remove_newline(account_number_for_balance);
+                        search_and_view_balance(account_number_for_balance);
+                        break;
+                    }
+                    case 5:
+                    {
+                        char account_number_for_deposit_funds[ACCOUNT_NUMBER_LENGTH + 1];
+                        printf("Enter the account number: ");
+                        fgets(account_number_for_deposit_funds, ACCOUNT_NUMBER_LENGTH + 1, stdin);
+                        remove_newline(account_number_for_deposit_funds);
+                        deposit_funds(account_number_for_deposit_funds);
+                        break;
+                    }
+                    case 6:
+                    {
+                        char account_number_for_withdraw_funds[ACCOUNT_NUMBER_LENGTH + 1];
+                        printf("Enter the account number: ");
+                        fgets(account_number_for_withdraw_funds, ACCOUNT_NUMBER_LENGTH + 1, stdin);
+                        remove_newline(account_number_for_withdraw_funds);
+                        withdraw_funds(account_number_for_withdraw_funds);
+                        break;
+                    }
+                    case 7:
+                    {
+                        admin_transfer_funds();
+                        break;
+                    }
+                    case 8:
+                    {
+                        char account_number_for_account_delete[ACCOUNT_NUMBER_LENGTH + 1];
+                        printf("Enter the account number: ");
+                        fgets(account_number_for_account_delete, ACCOUNT_NUMBER_LENGTH + 1, stdin);
+                        remove_newline(account_number_for_account_delete);
+                        deleteAccount(account_number_for_account_delete);
+                    }
+                    case 9:
+                    {
+                        char account_number_for_account_update[ACCOUNT_NUMBER_LENGTH + 1];
+                        printf("Enter the account number: ");
+                        fgets(account_number_for_account_update, ACCOUNT_NUMBER_LENGTH + 1, stdin);
+                        remove_newline(account_number_for_account_update);
+                        updateAccount(account_number_for_account_update);
+                    }
+                    case 10:
+                        logged_in = 0; // Logout
+                        break;
+                    default:
+                        printf("Invalid choice.\n");
+                    }
                 }
             }
             break;
@@ -250,7 +252,7 @@ void update_branch_account_balance(double amount)
     {
         fprintf(file, "%.2f\n", new_balance); // Write the new balance to the file
         fclose(file);
-        // printf("Branch account balance updated to: %.2f\n", new_balance);
+        printf("Branch account balance updated to: %.2f\n", new_balance);
     }
     else
     {
